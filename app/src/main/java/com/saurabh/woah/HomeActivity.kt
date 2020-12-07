4195fc46fcf798d0e88b58d729c481b3e5739a8f
SaurabhS-GWU <saurabhshetty7@gmail.com>
2020-12-07 11:58:02 -0500

    initial commit with splash, home and register screens and placeholder logos

---
+++ app/src/main/java/com/saurabh/woah/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.saurabh.woah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
