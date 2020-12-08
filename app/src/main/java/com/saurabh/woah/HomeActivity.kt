ee33bb9a0f662c02832113231addbd1af9c2e1f6
SaurabhS-GWU <saurabhshetty7@gmail.com>
2020-12-08 00:58:21 -0500

    Registration page layout added

--- app/src/main/java/com/saurabh/woah/HomeActivity.kt
+++ app/src/main/java/com/saurabh/woah/HomeActivity.kt
@@ -1,11 +1,18 @@
 package com.saurabh.woah
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        register.setOnClickListener {
+            val intent = Intent(this@HomeActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
